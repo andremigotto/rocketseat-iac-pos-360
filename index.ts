ea25381ff6cb7953ad4670c2a2af54f7d8d74d0c
Andre Migotto <migotto@Andres-Mac-mini.local>
2025-07-05 13:36:18 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+// Create an AWS resource (S3 Bucket)
+const bucket = new aws.s3.BucketV2("rocketseat-migotto-pulumi-teste", {
+    bucket: "rocketseat-migotto-pulumi-teste",
+    tags: {
+        IAC: "true"
+    },
+});
+
+const ecr = new aws.ecr.Repository("rocketseat-migotto-pulumi-teste", {
+    name: "rocketseat-migotto-pulumi-teste",
+    imageTagMutability: "IMMUTABLE",
+    tags: {
+        IAC: "true"
+    },
+});
+
+// Export the name of the bucket
+export const bucketName = bucket.id;
+export const bucketInfo = bucket.bucket;
+export const bucketArn = bucket.arn;
+
+export const ecrName = ecr.name;
+export const ecrRepositoryUrl = ecr.repositoryUrl;
+export const ecrArn = ecr.arn;
+
